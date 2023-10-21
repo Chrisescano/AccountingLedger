a42106874c3de92c231c81c0467ee68c552343c1
Christian Escano <escanochristian21@gmail.com>
2023-10-20 21:18:42 -0400

    Added: ledgerScreenPrompt method - allows user view ledger in different modes

--- src/main/java/org/pluralsight/Main.java
+++ src/main/java/org/pluralsight/Main.java
@@ -2,7 +2,8 @@ package org.pluralsight;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to your accounting ledger!");
-        homeScreenPrompt();
+        //homeScreenPrompt();
+        ledgerScreenPrompt();
     }
 
     public static void homeScreenPrompt() {
@@ -23,6 +24,31 @@ public class Main {
                 case 'X':
                     System.out.println("You want to exit from the program");
                     return;
+                default:
+                    System.out.println("Sorry, that is not a valid command. Please try again");
+            }
+        }
+    }
+
+    public static void ledgerScreenPrompt() {
+        while(true) {
+            System.out.print("Here are the commands you can do:\n  (A) - Display all entries\n" +
+                    "  (D) - Display only deposits\n  (P) - Display only payments\n  (R) - Filter ledger by defined values\n" +
+                    "  (H) - Go back to the home menu\nType command: ");
+            char command = ImprovedIO.getCharInput();
+            switch(command) {
+                case 'A':
+                    System.out.println("You want to display all entries");
+                    break;
+                case 'D':
+                    System.out.println("Display only deposits");
+                    break;
+                case 'P':
+                    System.out.println("Display only payments");
+                    break;
+                case 'R':
+                    System.out.println("You want to filter the ledger by defined values");
+                    break;
                 default:
                     System.out.println("Sorry, that is not a valid command. Please try again");
             }
