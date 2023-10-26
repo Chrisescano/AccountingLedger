7aa303dfd50e246e6160562027df31c1617b1b59
Christian Escano <escanochristian21@gmail.com>
2023-10-26 00:21:01 -0400

    Commit: LedgerScreenPanel class Added: actionlisteners to HomeMenuPanel buttons

--- src/main/java/org/pluralsight/gui/HomeMenuPanel.java
+++ src/main/java/org/pluralsight/gui/HomeMenuPanel.java
@@ -3,19 +3,50 @@ package org.pluralsight.gui;
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class HomeMenuPanel extends JPanel {
+public class HomeMenuPanel extends JPanel implements ActionListener {
 
     AppJFrame appJFrame;
-    JButton[] buttons = new JButton[4];
-    String[] buttonTitles = {"Add Deposit", "Add Payment", "Ledger Screen", "Quit Program"};
+    JButton addDeposit, addPayment, ledgerMenu, quitProgram;
 
     public HomeMenuPanel(AppJFrame appJFrame) {
         this.appJFrame = appJFrame;
-        this.setPreferredSize(appJFrame.getMinimumSize()); //sets size of JPanel - or else will be (0,0)
-        for(int i = 0; i < buttons.length; i++) {
-            buttons[i] = new JButton(buttonTitles[i]);
-            this.add(buttons[i]);
+        this.setPreferredSize(appJFrame.getPreferredSize()); //sets size of JPanel - or else will be (0,0)
+
+        addDeposit = new JButton("Add Deposit");
+        addPayment = new JButton("Add Payment");
+        ledgerMenu = new JButton("Ledger Menu");
+        quitProgram = new JButton("Quit Program");
+
+        addDeposit.addActionListener(this);
+        addPayment.addActionListener(this);
+        ledgerMenu.addActionListener(this);
+        quitProgram.addActionListener(this);
+
+        this.add(addDeposit);
+        this.add(addPayment);
+        this.add(ledgerMenu);
+        this.add(quitProgram);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == addDeposit) {
+            System.out.println("Deposits");
+        }
+
+        if(e.getSource() == addPayment) {
+            System.out.println("Payments");
+        }
+
+        if(e.getSource() == ledgerMenu) {
+            System.out.println("Ledger");
+        }
+
+        if (e.getSource() == quitProgram) {
+            System.out.println("Quit program");
         }
     }
 }
---
+++ src/main/java/org/pluralsight/gui/LedgerScreenPanel.java
@@ -0,0 +1,12 @@
+package org.pluralsight.gui;
+
+import javax.swing.*;
+
+public class LedgerScreenPanel extends JPanel {
+
+    AppJFrame appJFrame;
+
+    public LedgerScreenPanel(AppJFrame appJFrame) {
+        this.appJFrame = appJFrame;
+    }
+}
